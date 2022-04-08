b54609c2954efc5b6b74b04ef271792e046d284a
Yusufaky <44864683+Yusufaky@users.noreply.github.com>
2022-04-08 10:16:33 +0100

    2.6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <h1>Ola pela parte da App</h1>;
+  const title = "Hello World";
+  const subtitle = "This is my first React app";
+  const comments = [
+    { id: 1, text: "This is a comment" },
+    { id: 2, text: "This is another comment" },
+    { id: 3, text: "This is a third comment" },
+  ];
+
+  const loading = false;
+  const showComments = false;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{subtitle}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
 }
 
 export default App;
