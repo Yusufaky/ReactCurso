20bf62d396d28bd68d90605c3a26b32620ad39f1
Yusufaky <44864683+Yusufaky@users.noreply.github.com>
2022-05-13 21:21:42 +0100

    14

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
+import FeedbackItem from "./components/FeedbackItem";
+import Header from "./components/Header";
+
 function App() {
-  const title = "Hello World";
-  const subtitle = "This is my first React app";
-  const comments = [
-    { id: 1, text: "This is a comment" },
-    { id: 2, text: "This is another comment" },
-    { id: 3, text: "This is a third comment" },
-  ];
-
-  const loading = false;
-  const showComments = false;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{subtitle}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My APP</h1>
+        <FeedbackItem />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = { backgroundColor: bgColor, color: textColor };
+  return (
+    <header style={headerStyle}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
+  );
+}
+Header.defaultProps = {
+  text: "Header",
+  bgColor: "rgba(0,0,0,0.5)",
+  textColor: "#ff6a95",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+
+export default Header;
